strongswan/strongswan

commit 85ebf6abd4412d241fd55bc076b60f0a87931540

whitelist: Add error handling to socket reads and fix a memory leak
This now adds some state (basically a message buffer), but simplifies
error handling as we don't have to handle two potential failure paths
and could avoid some potential issues by still calling the blocking
read_all().

It also fixes a memory leak when clients disconnect.

src/libcharon/plugins/whitelist/whitelist_control.c
@@ -93,37 +93,50 @@ static void list(private_whitelist_control_t *this,
 }
 
 /**
+ * Information about a client connection.
+ */
+typedef struct {
+	private_whitelist_control_t *this;
+	whitelist_msg_t msg;
+	size_t read;
+} whitelist_conn_t;
+
+/**
  * Dispatch a received message
  */
 CALLBACK(on_read, bool,
-	private_whitelist_control_t *this, stream_t *stream)
+	whitelist_conn_t *conn, stream_t *stream)
 {
+	private_whitelist_control_t *this = conn->this;
 	identification_t *id;
-	whitelist_msg_t msg;
-	ssize_t n;
+	ssize_t len;
 
 	while (TRUE)
 	{
-		n = stream->read(stream, &msg, sizeof(msg), FALSE);
-		if (n <= 0)
+		while (conn->read < sizeof(conn->msg))
 		{
-			if (errno == EWOULDBLOCK)
+			len = stream->read(stream, (char*)&conn->msg + conn->read,
+							   sizeof(conn->msg) - conn->read, FALSE);
+			if (len <= 0)
 			{
-				break;
-			}
-			return FALSE;
-		}
-		if (n < sizeof(msg))
-		{
-			if (!stream->read_all(stream, ((char *)&msg) + n, sizeof(msg) - n))
-			{
+				if (errno == EWOULDBLOCK)
+				{
+					return TRUE;
+				}
+				if (len != 0)
+				{
+					DBG1(DBG_CFG, "whitelist socket error: %s", strerror(errno));
+				}
+				stream->destroy(stream);
+				free(conn);
 				return FALSE;
 			}
+			conn->read += len;
 		}
 
-		msg.id[sizeof(msg.id) - 1] = 0;
-		id = identification_create_from_string(msg.id);
-		switch (ntohl(msg.type))
+		conn->msg.id[sizeof(conn->msg.id) - 1] = 0;
+		id = identification_create_from_string(conn->msg.id);
+		switch (ntohl(conn->msg.type))
 		{
 			case WHITELIST_ADD:
 				this->listener->add(this->listener, id);
@@ -148,6 +161,7 @@ CALLBACK(on_read, bool,
 				break;
 		}
 		id->destroy(id);
+		conn->read = 0;
 	}
 
 	return TRUE;
@@ -156,7 +170,12 @@ CALLBACK(on_read, bool,
 CALLBACK(on_accept, bool,
 	private_whitelist_control_t *this, stream_t *stream)
 {
-	stream->on_read(stream, on_read, this);
+	whitelist_conn_t *conn;
+
+	INIT(conn,
+		.this = this,
+	);
+	stream->on_read(stream, on_read, conn);
 	return TRUE;
 }
 

CodeLinaro-mirror/femto_fsm_external_opensource_strongswan

commit 0bcd175918c6038740aae213899dd16475307118

whitelist: Fix deadlock when handling client disconnection
Calling stream_t::destroy from the stream_t::on_read callback will
block the thread in watcher_t::remove because the FD is currently "in
callback".  A similar issue was fixed in the lookip plugin with
961409b66858 ("lookip: Disconnect asynchronously to avoid dead-locking
watcher unregistration").

Fixes: 85ebf6abd441 ("whitelist: Add error handling to socket reads and fix a memory leak")

src/libcharon/plugins/whitelist/whitelist_control.c
@@ -25,6 +25,7 @@
 
 #include <daemon.h>
 #include <collections/linked_list.h>
+#include <processing/jobs/callback_job.h>
 
 #include "whitelist_msg.h"
 
@@ -102,6 +103,40 @@ typedef struct {
 } whitelist_conn_t;
 
 /**
+ * Information needed for async disconnect job.
+ */
+typedef struct {
+	whitelist_conn_t *conn;
+	stream_t *stream;
+} disconnect_data_t;
+
+/**
+ * Asynchronous callback to disconnect client
+ */
+CALLBACK(disconnect_async, job_requeue_t,
+	disconnect_data_t *data)
+{
+	data->stream->destroy(data->stream);
+	free(data->conn);
+	return JOB_REQUEUE_NONE;
+}
+
+/**
+ * Disconnect a connected client
+ */
+static void disconnect(whitelist_conn_t *conn, stream_t *stream)
+{
+	disconnect_data_t *data;
+
+	INIT(data,
+		.conn = conn,
+		.stream = stream,
+	);
+	lib->processor->queue_job(lib->processor,
+			(job_t*)callback_job_create(disconnect_async, data, free, NULL));
+}
+
+/**
  * Dispatch a received message
  */
 CALLBACK(on_read, bool,
@@ -127,8 +162,7 @@ CALLBACK(on_read, bool,
 				{
 					DBG1(DBG_CFG, "whitelist socket error: %s", strerror(errno));
 				}
-				stream->destroy(stream);
-				free(conn);
+				disconnect(conn, stream);
 				return FALSE;
 			}
 			conn->read += len;